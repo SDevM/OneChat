445678cf35ddd15a122d097520d5473855acb5f3
SDevM <lolicongress@outlook.com>
2023-02-12 23:29:42 -0500

    Epress test

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 require("dotenv").config()
 const { PORT } = process.env
 const crypto = require("crypto")
+const express = require("express")()
 const http = require("http")
 const { Server } = require("socket.io")
-const httpServer = http.createServer()
+const httpServer = http.createServer(express)
 httpServer.listen(PORT)
 const ioSocketServer = new Server(httpServer)
 
@@ -11,6 +12,8 @@ const msgs = []
 const clients = new Map()
 
 if (ioSocketServer) console.log("Socket Server Operational")
+
+express.get("/")
 
 ioSocketServer.on("connection", (socket) => {
   // console.log(`New conneciton: ${socket.handshake.address}`)
